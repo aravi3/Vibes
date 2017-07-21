d0799345a5e51c2a14edd7b91349d6542f5c053a
Arvind Ravi <ar2au@virginia.edu>
2017-07-21 13:22:19 -0700

    Edit AWS bucket region

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,3 @@ user5 = User.create!(username: "GuestUser5", email: "guest5@demo.com", password:
 genre1 = Genre.create!(name: "Electronic")
 genre2 = Genre.create!(name: "Hip-Hop")
 genre3 = Genre.create!(name: "Folk")
-
-song1 = Song.create!(
-  user_id: user1.id,
-  genre_id: genre3.id,
-  title: "Cotton Eye Joe",
-  artist: user1.username,
-  image: File.new("/Users/arvindravi/Desktop/Songs/CottonEyeJoe/cotton_eye_joe.jpg"),
-  track: File.new("/Users/arvindravi/Desktop/Songs/CottonEyeJoe/cotton_eye_joe.mp3")
-)
